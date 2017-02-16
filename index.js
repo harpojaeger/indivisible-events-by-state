aea12ceaa34c31a6a3f8479e50e581aa3c975dfa
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-02-16 15:17:46 -0700

    Log state before beginning search

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 }));
 
 app.get('/:state', function(req, res) {
+  console.log('Looking for events in ' + req.params.state)
   var theRightEvents = []
   var urls = []
   for(page = 1; page <19; page++){
